bafaeca6839fbe841f951f211e46e2ceb79af980
unknown <r.zinatulinas@Core-i7-13.AS.local>
2014-05-09 19:17:47 +0300

    a

--- source/TestIt/Transform/RandomString.cs
+++ source/TestIt/Transform/RandomString.cs
@@ -8,19 +8,26 @@ namespace TestIt.Transform
     public class RandomString : ITransform
     {
         static string LoadString { get; set; }
+        static object string_lock = new object();
 
         public Tests.Request Transform(Tests.Request request)
         {
             if (string.IsNullOrEmpty(RandomString.LoadString))
             {
-                Console.WriteLine("Building string...");
-                StringBuilder sb = new StringBuilder();
-                var part = Helpers.GetRandomString();
-                for (int i = 0; i < 20000 / part.Length; i++)
+                lock (string_lock)
                 {
-                    sb.Append(part);
+                    if (string.IsNullOrEmpty(RandomString.LoadString))
+                    {
+                        Console.WriteLine("Building string...");
+                        StringBuilder sb = new StringBuilder();
+                        var part = Helpers.GetRandomString();
+                        for (int i = 0; i < 20000 / part.Length; i++)
+                        {
+                            sb.Append(part);
+                        }
+                        RandomString.LoadString = sb.ToString();
+                    }
                 }
-                RandomString.LoadString = sb.ToString();
             }
             request.Url = request.Url.Replace("{random_string}", Helpers.GetRandomString());
             if (!string.IsNullOrEmpty(request.BodyBytes))
